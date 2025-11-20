83c14762d153d5556f91b872ce0502231913cf61
seete001 <sepehr.etmd@gmail.com>
2025-11-20 17:23:52 +0100

    changed the directory of db

---
+++ dbase.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+def db_init(filepath):
+    conn = sqlite3.connect('db/database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            phone TEXT,
+            message TEXT
+        )
+    ''')
